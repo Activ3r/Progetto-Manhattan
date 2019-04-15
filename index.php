99359ccdfa827e6ad05eee844d382d55aed5a01c
Emanuele Piatti <emanuelepiatti@gmail.com>
2019-04-15 12:19:22 +0200

    update

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <title>LDAP tool</title>
 
     <!--include-->
+    <link rel="stylesheet" type="text/css" href="all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!--/include-->
 </head>
 <body>
@@ -30,6 +34,30 @@
         else {
             return FALSE;
         }
+
+    }
+
+    function login(){
+        ?>
+        <div class="wrapper fadeInDown">
+            <div id="formContent">
+            <!-- Tabs Titles -->
+
+            <!-- Icon -->
+            <div class="fadeIn first">
+                <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+            </div>
+
+            <!-- Login Form -->
+            <form>
+            <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+            <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+            <input type="submit" class="fadeIn fourth" value="Log In">
+            </form>
+
+            </div>
+        </div>
+    <?php
 
     }
 	
