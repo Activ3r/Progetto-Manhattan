8b06a41d97b3d8e5ba2979ec04979d8d026de5d0
Emanuele Piatti <emanuelepiatti@gmail.com>
2019-04-16 10:16:24 +0200

    update
    -collegato sistema ldap-php ad un server di test
    FUNZIA

--- index.php
+++ index.php
@@ -20,8 +20,7 @@
         $ldap_dn = "cn=read-only-admin,dc=example,dc=com";
         $ldap_password = "password";
         
-        $ldap_con = ldap_connect("ldap.forumsys.com");
-        
+        $ldap_con = ldap_connect("ldap.forumsys.com");       
         ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
         
         if(ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
