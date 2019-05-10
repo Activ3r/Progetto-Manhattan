a0058cd44f55dc583a6d8c818273160518a1af35
Emanuele Piatti <emanuelepiatti@gmail.com>
2019-05-10 13:48:49 +0200

    -update ldap_connect
    -commentato i dati di test per la funzione ldap_connect
    -configurato la funzione ldap_connect con i dati reali per la connessione

--- index.php
+++ index.php
@@ -20,6 +20,22 @@
 require('header.php');
 
   function ldap_connection(){
+    $ldap_dn = "cn=dominio-SERVER-PROGETTO-CA,dc=dominio,dc=local";
+    $ldap_password = "Abc123!";
+    
+    $ldap_con = ldap_connect("127.0.0.1");       
+    ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+    
+    if(ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
+      #return TRUE;
+      echo "Connected";
+    } 
+    else {
+      #return FALSE;
+      echo "Error";
+    }
+
+    /* TEST
     $ldap_dn = "cn=read-only-admin,dc=example,dc=com";
     $ldap_password = "password";
     
@@ -34,6 +50,7 @@ require('header.php');
       #return FALSE;
       echo "Error";
     }
+    */
   }
 
   function get_users_list() {  
