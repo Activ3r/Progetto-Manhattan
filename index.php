32ef7af1c623bf8415269f495e56929663f6e367
Emanuele Piatti <emanuelepiatti@gmail.com>
2019-05-15 10:47:44 +0200

    update
    -creato pagina "crea utente"

--- index.php
+++ index.php
@@ -20,24 +20,23 @@
 require('header.php');
 
   function ldap_connection(){
-    $ldap_dn = "cn=dominio-SERVER-PROGETTO-CA,dc=dominio,dc=local";
-    $ldap_password = "Abc123!";
+    // $ldap_dn = "cn=dominio-SERVER-PROGETTO-CA,dc=dominio,dc=local";
+    // $ldap_password = "Abc123!";
     
-    $ldap_con = ldap_connect("127.0.0.1");       
-    ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+    // $ldap_con = ldap_connect("127.0.0.1");       
+    // ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
     
-    if(ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
-      echo "Connected";
-      return TRUE;
+    // if(ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
+    //   echo "Connected";
+    //   return TRUE;
       
-    } 
-    else {
-      echo "Error";
-      return FALSE;
+    // } 
+    // else {
+    //   echo "Error";
+    //   return FALSE;
       
-    }
+    // }
 
-    /* TEST
     $ldap_dn = "cn=read-only-admin,dc=example,dc=com";
     $ldap_password = "password";
     
@@ -45,14 +44,14 @@ require('header.php');
     ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
     
     if(ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
-      #return TRUE;
       echo "Connected";
+      return TRUE;
     } 
     else {
-      #return FALSE;
       echo "Error";
+      return FALSE;
     }
-    */
+    
   }
 
   function get_users_list() {  
