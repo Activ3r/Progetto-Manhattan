e679fb86717e89ff97d7551208ae75227d87f666
Ordnas17 <st.rappo17@gmail.com>
2019-05-16 11:44:11 +0200

    Update
    Creato Oggetto ldap contenente le funzioni  get_user_list e ldap_status.
    Eliminato file getUserList e ldap_connection

--- index.php
+++ index.php
@@ -17,51 +17,17 @@
 
 <?php
 // imported only php file
-require_once('ldap_connection.php');
-require_once('getUserList.php');
+require_once('ldap.php');
 
 //header
 require_once('header.php');
 
 
+$prova = new ldap;
 
-  function ldap_connection(){
-    $ldap_dn = "cn=dominio-SERVER-PROGETTO-CA,dc=dominio,dc=local";
-    $ldap_password = "Abc123!";
-    
-    $ldap_con = ldap_connect("127.0.0.1");       
-    ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
-    
-    if(ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
-      #return TRUE;
-      echo "Connected";
-    } 
-    else {
-      #return FALSE;
-      echo "Error";
-    }
+$prova->get_users_list();
 
-    /* TEST
-    $ldap_dn = "cn=read-only-admin,dc=example,dc=com";
-    $ldap_password = "password";
-    
-    $ldap_con = ldap_connect("ldap.forumsys.com");       
-    ldap_set_option($ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
-    
-    if(ldap_bind($ldap_con, $ldap_dn, $ldap_password)) {
-      #return TRUE;
-      echo "Connected";
-    } 
-    else {
-      #return FALSE;
-      echo "Error";
-    }
-    */
-  }
 
-  function get_users_list() {  
-    #TODO:  ottenere lista di tutti gli utenti con i permessi e i dati
-  }
 
   function get_grups_list() {
     #TODO:  ottenere lista di tutti i gruppi con i permessi e i dati
@@ -90,7 +56,7 @@ require_once('header.php');
   </tr>
   <tr>
     <td>LDAP</td>
-    <td><?php ldap_connection()?></td>
+    <td> <?php $prova->ldap_status() ?> </td>
   </tr>
 </table>
 
---
+++ ldap.php
@@ -0,0 +1,67 @@
+<?php
+
+class ldap{
+
+    #Original
+    // private $ldap_dn = "cn=dominio-SERVER-PROGETTO-CA,dc=dominio,dc=local";
+    // private $ldap_password = "Abc123!";
+
+    #Test
+    private $ldap_dn = "cn=read-only-admin,dc=example,dc=com";
+    private $ldap_password = "password";
+    private $ldap_con;
+    private $ldap_status=true;
+
+    function __construct(){
+        
+        #Original
+        // $ldap_con = ldap_connect("127.0.0.1");
+        
+        #Test
+    $this->$ldap_con = ldap_connect("ldap.forumsys.com");
+    echo $this->$ldap_con;
+
+        ldap_set_option($this->$ldap_con, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+        if ($this->$ldap_con === false) {
+            die("No LDAP Connection!");
+        }
+
+        if(!ldap_bind($this->$ldap_con, $ldap_dn, $ldap_password)){
+            die("Error LDAP Connection!");
+            $this->$ldap_status = false;
+        }
+
+    }
+
+    function get_users_list() {  
+        echo $this->$ldap_con;
+        #TODO:  ottenere lista di tutti gli utenti con i permessi e i dati
+        
+        $filter = "(cn=*)";
+        #original
+        // $result =  ldap_search($ldap_con, "dc=dominio,dc=local",$filter ) or exit ("Unable to search");
+
+        #Test
+        $result = ldap_search($this->$ldap_con,"dc=example,dc=com",$filter) or exit("Unable to search");
+        
+        $entries = ldap_get_entries($this->$ldap_con,$result);
+
+        print "<pre>";
+        print_r($entries);
+        print "</pre>";
+    
+    }
+
+    function ldap_status(){
+        if($this->$ldap_status){
+            echo "Ok";
+        }
+        else {
+            echo "Error";
+        }
+
+    }
+
+}
+?>
